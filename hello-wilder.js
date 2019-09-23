d1f7c6dcca94502c436909ca6dd8fd9665821199
Solène Tlr <53545458+solenetlr@users.noreply.github.com>
2019-09-23 11:05:26 +0200

    Create hello-wilder.js

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const Film = "Le diable s'habille en Prada";
+const Realisator = 'David Frankel';
+const year = 2006;
+const message = Film  + ' réalisé par ' + Realisator  + ' est sorti en ' + year + ' !' ;
+
+// Display a popup
+alert(message);
